592b69e0d2010baf46bf550f93628d358dc4c362
Lưu Đạt <luudat@users.noreply.github.com>
2018-10-03 06:26:32 +0700

    Renew Git Server

---
+++ SorceCode/Android/Demo/HabitTracker2/app/src/main/java/com/example/datlu/habittracker2/MenuActivity.java
@@ -0,0 +1,47 @@
+package com.example.datlu.habittracker2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuActivity extends AppCompatActivity implements MenuRecyclerViewAdapter.ItemClickListener {
+
+    MenuRecyclerViewAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        List<MenuItem> data = new ArrayList<>();
+        MenuItem item = new MenuItem("Personal", "12", R.drawable.ps);
+        data.add(item);
+        item = new MenuItem("WORK", "12", R.drawable.work);
+        data.add(item);
+        item = new MenuItem("MEET", "12", R.drawable.meet);
+        data.add(item);
+        item = new MenuItem("HOME", "12", R.drawable.home);
+        data.add(item);
+        item = new MenuItem("PRIVATE", "12", R.drawable.privateic);
+        data.add(item);
+        item = new MenuItem("ADD NEW", "", R.drawable.ps);
+        data.add(item);
+
+        RecyclerView recyclerView = findViewById(R.id.rvMenu);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new MenuRecyclerViewAdapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+
+    }
+}
