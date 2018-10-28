ae4700be39d6af63e467bbeb9683d2faf55cb095
Phạm Thanh Tùng <tungptse61628@fpt.edu.vn>
2018-10-28 20:05:13 +0700

    common web

---
+++ SorceCode/dev_web/habitweb/src/main/java/com/example/habitweb/HabitWebApplication.java
@@ -0,0 +1,12 @@
+package com.example.habitweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitWebApplication.class, args);
+    }
+}
