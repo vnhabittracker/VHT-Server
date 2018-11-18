d5c597b2bc0309ad3c20ec5e59a70c271b38626f
Phạm Thanh Tùng <tungptse61628@fpt.edu.vn>
2018-11-18 16:19:50 +0700

    connect

--- SorceCode/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/api/VnHabitApiUtils.java
+++ SorceCode/dev_android/HabitTracker/app/src/main/java/habit/tracker/habittracker/api/VnHabitApiUtils.java
@@ -5,7 +5,8 @@ import habit.tracker.habittracker.api.service.VnHabitApiService;
 
 public class VnHabitApiUtils {
     //    public static final String BASE_URL = "http://192.168.42.139/php_rest_vnhabit/api/";
-    public static final String BASE_URL = "http://192.168.56.1/php_rest_vnhabit/api/";
+    // TungPT: public static final String BASE_URL = "http://192.168.182.2:8080/php_rest_vnhabit/api/";
+    public static final String BASE_URL = "http://192.168.1.65/php_rest_vnhabit/api/";
 
     public static VnHabitApiService getApiService() {
         return RetrofitClient.getClient(BASE_URL).create(VnHabitApiService.class);
